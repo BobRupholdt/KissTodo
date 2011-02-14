5c433fbe02903254270933dceddd0f85dff83445
Massimo Barbieri <barmassimo@gmail.com>
2011-02-14 11:01:05 +0100

    cleanup test page

--- todo/views.py
+++ todo/views.py
@@ -26,26 +26,13 @@ from random import choice
 from models import *
   
 def test_page(request):
-    if 'op' in request.POST and request.POST['op']=='create list':
-        new_list=List(name=request.POST['add'])
-        new_list.save()
-        return HttpResponseRedirect(reverse(test_page))
-        
-    if 'op' in request.POST and request.POST['op']=='create todo':
-        new_todo=Todo()
-        new_todo.priority = 1
-        new_todo.list = choice(List.objects.all())
-        new_todo.description=request.POST['add']
-        new_todo.save()
-        return HttpResponseRedirect(reverse(test_page))
-        
-    if 'op' in request.POST and request.POST['op']=='clear data':
-        for tl in List.objects.all(): tl.delete()
-        for t in Todo.objects.all(): t.delete()
-        return HttpResponseRedirect(reverse(test_page))    
+    #if 'op' in request.POST and request.POST['op']=='clear data':
+    #    for tl in List.objects.all(): tl.delete()
+    #    for t in Todo.objects.all(): t.delete()
+    #    return HttpResponseRedirect(reverse(test_page))    
 
     if 'op' in request.POST and request.POST['op']=='simulate error':
-        raise Exception("Error!")
+        raise Exception("Simulated Error!")
     
     return render_to_response('todo/test_page.html', 
         RequestContext(request, {'media_root':settings.MEDIA_ROOT, 'lists':List.objects, 'todos':Todo.objects,}))
